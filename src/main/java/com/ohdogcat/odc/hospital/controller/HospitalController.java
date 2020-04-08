c344f51e2d1c6d5621baa4d683b216ed9b6cef28
Rin <Rin@EX58>
2020-04-08 17:44:07 +0900

    2020 0408 진우 차트오류수정

--- src/main/java/com/ohdogcat/odc/hospital/controller/HospitalController.java
+++ src/main/java/com/ohdogcat/odc/hospital/controller/HospitalController.java
@@ -1,7 +1,9 @@
 package com.ohdogcat.odc.hospital.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HospitalController {
 
 	@RequestMapping("chart.ho")
