963375a9258d0af7340bed57c9ff36cf5dc40655
Rin <Rin@EX58>
2020-04-09 19:12:14 +0900

    2020 04 09 병원 회원가입

--- src/main/java/com/ohdogcat/odc/homepage/member/controller/HMemberController.java
+++ src/main/java/com/ohdogcat/odc/homepage/member/controller/HMemberController.java
@@ -1,12 +1,18 @@
 package com.ohdogcat.odc.homepage.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ohdogcat.odc.homepage.member.service.HMemberService;
+
 @Controller
 public class HMemberController {
+	
+	@Autowired
+	private HMemberService hmService;
 
 	@RequestMapping("hSignin.do")
 	public String hInsertForm() {
@@ -15,9 +21,11 @@ public class HMemberController {
 	
 	@ResponseBody
 	@RequestMapping("hIdCheck.do")
-	public String hIdChekck() {
+	public String hIdCheck(String userId) {
 		
-		return "";
+		int result = hmService.hIdCheck(userId);
+
+		return Integer.valueOf(result).toString();
 		
 	}
 	
