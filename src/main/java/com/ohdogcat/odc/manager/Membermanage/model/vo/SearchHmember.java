f784945c9b0c50ad625ba1c2d9af33477b27d0a8
Rin <Rin@EX58>
2020-04-14 16:57:38 +0900

    2020 04 14 진우 병원관리

---
+++ src/main/java/com/ohdogcat/odc/manager/Membermanage/model/vo/SearchHmember.java
@@ -0,0 +1,36 @@
+package com.ohdogcat.odc.manager.Membermanage.model.vo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchHmember {
+	private int category;
+	private String search;
+	public SearchHmember() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public SearchHmember(int category, String search) {
+		super();
+		this.category = category;
+		this.search = search;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	@Override
+	public String toString() {
+		return "SrearchHmember [category=" + category + ", search=" + search + "]";
+	}
+	
+	
+}
